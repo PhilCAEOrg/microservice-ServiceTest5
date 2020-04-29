659e94c30a01dc43c88d5d50bc06767274dadecf
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-04-29 17:17:21 +0000

    Generated microservice version 0.0.1

---
+++ db/ServiceTest5_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  
+-- Automatically generated sql script for the service ServiceTest5, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table testtable.
+--
+CREATE TABLE .testtable (
+  name varchar ,
+CONSTRAINT name_PK PRIMARY KEY (name)
+ 
+);
+
+
+
